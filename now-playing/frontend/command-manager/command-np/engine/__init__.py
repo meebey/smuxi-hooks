80ae1eb44e1a0640b7541662be49576dba93090d
jamesaxl <axlrose112@gmail.com>
2015-01-19 22:15:35 +0000

    now-playing: rewrite in Python

---
+++ now-playing/frontend/command-manager/command-np/engine/__init__.py
@@ -0,0 +1,27 @@
+#-*- coding: utf-8 -*-
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# Version 2, December 2004
+#
+# Copyright (c) 2015 James Axl <axlrose112@gmail.com>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
+
+from mprisplayer import MprisPlayer
+
+#BEFORE ADD ANY PLAYER TO 'MPLAS' TUPLE BE SURE PLEASE THAT IT SUPPORTS MPRIS2
+MPLAS = ('vlc', 
+        'rhythmbox', 
+        'clementine', 
+        'spotify', 
+        'tomahawk', 
+        'amarok', 
+        'banshee')
+
