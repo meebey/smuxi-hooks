fcae8d3108572ca497eb7a623beefd1627daf60a
Mirco Bauer <meebey@meebey.net>
2015-02-04 23:49:35 +0100

    deine-mudda: new entry

--- deine-mudda/frontend/command-manager/command-dm/deine-mudda.sh
+++ deine-mudda/frontend/command-manager/command-dm/deine-mudda.sh
@@ -48,6 +48,7 @@ DM[20]="Deine Mutter ist ein Funkloch."
 DM[21]="Deine Mutter dreht Quadrate bei Tetris."
 DM[22]="Deine Mutter stürzt öfter ab als Windows."
 DM[23]="Deine Mutter arbeitet bei IKEA als unterste Schublade."
+DM[24]="Deine Mutter ist so hässlich bei ihr wird eingebrochen um die Vorhänge zu schließen."
 
 IDX=$(shuf -i 0-${#DM[@]} -n 1)
 echo "ProtocolManager.Command /say ${DM[$IDX]}"
